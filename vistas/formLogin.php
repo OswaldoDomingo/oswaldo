adb22b5850f16d793f9777732c2f7f8789945db6
OswaldoGSDAW <147555767+OswaldoGSDAW@users.noreply.github.com>
2023-11-11 19:25:54 +0100

    Inicio proyecto

---
+++ vistas/formLogin.php
@@ -0,0 +1,5 @@
+<form action="" method="post">
+    <input type="email" name="correoLogin" id="">
+    <input type="password" name="contrasenyaLogin" id="">
+    <input type="submit" value="" name="enviarLogin">
+</form>
